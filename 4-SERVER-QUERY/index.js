cb713e33f4794ca47722bf2b9d909771f7a04ef7
100pearlcent <100pearlcent@naver.com>
2022-08-25 21:15:24 +0900

    💡 Fetch with argument

--- 4-SERVER-QUERY/index.js
+++ 4-SERVER-QUERY/index.js
@@ -1,9 +1,13 @@
 const database = require('./database')
 const { ApolloServer, gql } = require('apollo-server')
-​
+const { supplies } = require('./database')
+
 const typeDefs = gql`
     type Query {
         teams: [Team]
+        team(id: Int): Team
+        equipments: [Equipments]
+        supplies: [Supply]
     }
     type Team {
         id: Int
@@ -14,15 +18,32 @@ const typeDefs = gql`
         cleaning_duty: String
         project: String
     }
+    type Equipment {
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+  }
+  type Supply {
+        id: String,
+        team: Int
+  }
 `
+
 const resolvers = {
   Query: {
-    teams: () => database.teams
+    teams: () => database.teams,
+    team: (parent, args, context, info) => database.teams
+      .filter((team) => {
+        return team.id == args.id
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies
   }
 }
 ​
 const server = new ApolloServer({ typeDefs, resolvers })
 ​
 server.listen().then(({ url }) => {
-console.log(`🚀  Server ready at ${url}`)
+console.log(`🚀 서버 준비 완료 ➡️ ${url}`)
 })
