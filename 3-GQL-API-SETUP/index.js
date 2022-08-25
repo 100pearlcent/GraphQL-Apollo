45be3d16d6cfd625b7c8c208179a6d1931f47d05
100pearlcent <100pearlcent@naver.com>
2022-08-25 17:39:46 +0900

    💡 Build Apollo Server

--- 3-GQL-API-SETUP/index.js
+++ 3-GQL-API-SETUP/index.js
@@ -1,4 +1,31 @@
 // ▶️ nodemon index.js
 
 const database = require('./database')
-console.log(database)
+
+
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    teams: () => database.teams
+  }
+}
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => {
+console.log(`🚀 서버 준비 완료 ➡️ ${url}`)
+})
