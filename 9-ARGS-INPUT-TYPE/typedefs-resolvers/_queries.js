08cf83c9e10c2b26bb384576f2d61035665bdf48
100pearlcent <100pearlcent@naver.com>
2022-08-29 15:01:18 +0900

    🧹Fix Up Typo

--- 9-ARGS-INPUT-TYPE/typedefs-resolvers/_queries.js
+++ 9-ARGS-INPUT-TYPE/typedefs-resolvers/_queries.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server')
 
 const typeDefs = gql`
     type Query {
-        people: [People],
+        people: [People]
         peopleFiltered(
             team: Int,
             sex: Sex,
